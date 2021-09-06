391da869a21286270d00658f3cc586f1b1304659
widada <wida.skydev@gmail.com>
2021-09-06 15:31:06 +0700

    edit pinChecker

--- app/helpers.php
+++ app/helpers.php
@@ -7,6 +7,8 @@ function pinChecker($pin) {
     $userId = auth()->user()->id;
     $wallet = Wallet::find($userId);
 
+    if ($wallet == null) return false;
+
     if (Hash::check($pin, $wallet->pin)) return true;
     return false;
 }
