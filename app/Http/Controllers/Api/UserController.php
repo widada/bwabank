b631443b5e619d1912718956fd339cd8c6a5f456
widada <wida.skydev@gmail.com>
2021-09-07 09:27:07 +0700

    edit /users

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -16,6 +16,10 @@ class UserController extends Controller
         $wallet = Wallet::where('user_id', $user->id)->first();
         
         $userDatabase = User::find($user->id);
+        $userDatabase->profile_picture = $userDatabase->profile_picture ? 
+            url('storage/'.$userDatabase->profile_picture) : "";
+        $userDatabase->ktp = $userDatabase->ktp ? 
+            url('storage/'.$userDatabase->ktp) : "";
         $userDatabase->balance = $wallet->balance;
 
         return response()->json($userDatabase);
