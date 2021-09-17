f0b50523d4356cf459ea0623069fe5852d2e77dc
widada <wida.skydev@gmail.com>
2021-09-17 09:43:41 +0700

    add icon

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -22,7 +22,7 @@
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-          <a href="" class="nav-link">
+          <a href="/admin" class="nav-link">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>
               Dasboard
